1eada6bc188f60c9e31800628f1c5e93d4ac7812
Elia Sommerlad <Elia@Elias-MacBook-Pro.local>
2015-04-02 09:17:38 +0800

    Added button and .hide function

--- js/application.js
+++ js/application.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
 
+
+$(".button").click(function (){
+	$(".pageOne").hide(1000);
+	console.log("hello");
+	// $("pageTwo").show();
+});
+
+
+// function onlyShowCurrentSlide(currentSlideNum) {
+
+// 	$(".pageOne").hide();
+
+// 	currentPage = $(".pageTwo") [currentSlideNum];
+// 	$(currentPage).show();
+// }
+
+// $("#next").click(function()){
+// 	currentSlideNum++;
+// 	onlyShowCurrenPage(currentSlideNum);
+
+// });
+
+// $(document).on('click', '.answer', function(){
+// 	var=answer = $(this).attr()
+// }
 });
